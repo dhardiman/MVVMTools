8a7643c46269ea9cb80c096c7ff90f879a0e8b16
David Hardiman <david_hardiman@yahoo.co.uk>
2019-06-04 13:36:06 +0100

    Explicit dependencies on Bond and ReactiveKit

--- Package.swift
+++ Package.swift
@@ -9,10 +9,11 @@ let package = Package(
       .library(name: "MVVM", targets: ["MVVM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/DeclarativeHub/Bond", .upToNextMajor(from: "7.5.0")),        
+        .package(url: "https://github.com/DeclarativeHub/Bond", .upToNextMajor(from: "7.5.0")),
+        .package(url: "https://github.com/DeclarativeHub/ReactiveKit", .upToNextMajor(from: "3.12.2"))
     ],
     targets: [
-      .target(name: "MVVM", dependencies: []),
+      .target(name: "MVVM", dependencies: ["Bond", "ReactiveKit"]),
       .testTarget(name: "MVVMTests", dependencies: ["MVVM"])
     ],
     swiftLanguageVersions: [.v5]
